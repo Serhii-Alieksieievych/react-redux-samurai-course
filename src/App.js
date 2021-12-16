aae6167728839fb6c71d8457dc86434f926105aa
Serhii Alieksieievych <kajot1863@gmail.com>
2021-12-16 23:45:25 +0200

    observer was created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 import './App.css';
 
@@ -11,43 +11,32 @@ import News from './components/News/News';
 import Music from './components/Music/Music';
 import Settings from './components/Settings/Settings';
 
-const dialogsArr = [
-  {id:1, name:'Oppos'},
-  {id:2, name:'Kotossum'},
-  {id:3, name:'Huhol'},
-]
-
-const messagesArr = [
-  {id:1, message:'Hi'},
-  {id:2, message:'Sam hi'},
-  {id:3, message:'Net ti hi'},
-  {id:4, message:'Nihuja, ti hi'},
-  {id:5, message:'Ta ne, tochno ti hi'},
-]
-
-const postsArr = [
-  {id:1, message:`post-opost` , likesCount:0},
-  {id:2, message:`post-pos`, likesCount:0},
-  {id:3, message:`post-opos`, likesCount:0},
-]
-
-const App = () => {
+const App = ({ appState, addPost, changeCurrentPostArea, sendMessage, changeCurrentMessage }) => {
+  const { dialogsState, profileState} = appState;
   return (
-    <BrowserRouter>
-      <div className="app-wrapper">
+    <div className="app-wrapper">
       <Header />
       <Navbar />
       <div className="app-wrapper-content">
         <Routes>
-          <Route path="/profile" element={<Profile postsArr={postsArr} />}/>
-          <Route path="/dialogs" element={<Dialogs dialogsArr={dialogsArr} messagesArr={messagesArr}/>}/>
+          <Route path="/profile" element={<Profile profileState={profileState} addPost={addPost} changeCurrentPostArea={changeCurrentPostArea}/>}/>
+          <Route
+            path="/dialogs"
+            element={
+              <Dialogs
+                dialogsState={dialogsState}
+                messagesArr={dialogsState.messagesArr}
+                sendMessage={sendMessage}
+                changeCurrentMessage={changeCurrentMessage}
+              />
+            }
+          />
           <Route path="/news" element={<News />}/>
           <Route path="/music" element={<Music />}/>
           <Route path="/settings" element={<Settings />}/>
         </Routes>
       </div>
     </div>
-    </BrowserRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,16 +1,31 @@
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import state from './Redux/state';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import { addPost, changeCurrentPostArea, sendMessage, changeCurrentMessage } from './Redux/state';
+import { subscribe } from './Redux/state';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
+export const rerenderEntrieTree = (state) => {
+    ReactDOM.render(
+        <React.StrictMode>
+          <BrowserRouter>
+            <App
+                appState={state}
+                addPost={addPost}
+                changeCurrentPostArea={changeCurrentPostArea}
+                sendMessage={sendMessage}
+                changeCurrentMessage={changeCurrentMessage}
+            />
+          </BrowserRouter>
+        </React.StrictMode>,
+        document.getElementById('root')
+    );      
+}
+subscribe(rerenderEntrieTree);
+rerenderEntrieTree(state);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
