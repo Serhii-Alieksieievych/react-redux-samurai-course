05f75799632742782e4594de2c747aec51c1bd27
Serhii Alieksieievych <kajot1863@gmail.com>
2021-12-23 13:54:09 +0200

    preloader was added in header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import classes from './Header.module.css';
-
-const Header = ( {isLogged, email, id} ) => {
+import userPhoto from "../../assets/img/Opossums.jpg";
+import { Grid } from 'svg-loaders-react';
+const Header = ( {isLogged, email, id, smallAvatarSRC, isFetching} ) => {
     return (
         <header className={classes.header}>
             <img src="https://key0.cc/images/preview/83810_9affe954e1ae5d956f374d1a3526e6fc.png" alt="logo" className="logo" />
-            {isLogged ? <div>{email} You are logged {id}</div> : <div>Log in</div>}
+            {isFetching ? <Grid className={classes.preloader} />:isLogged 
+            ? <div>
+                {smallAvatarSRC
+                    ? <img src={smallAvatarSRC} alt="small_avatar" className={classes.ava}/>
+                    : <img src={userPhoto} alt="small_avatar" className={classes.ava} />}
+                {email} You are logged {id}
+                </div> 
+            : <div>Log in</div>}
         </header>
     )
 }
